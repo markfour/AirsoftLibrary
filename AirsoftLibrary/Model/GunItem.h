58558400e6df909f266a950a0fe03a6db872594b
markfour <axhfaro@gmail.com>
2018-03-29 18:32:09 +0900

    GunItemを追加

---
+++ AirsoftLibrary/Model/GunItem.h
@@ -0,0 +1,13 @@
+//
+//  GunItem.h
+//  AirsoftLibrary
+//
+//  Created by kazumi hayashida on 2018/03/29.
+//  Copyright © 2018年 kh.inc.d. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GunItem : NSObject
+
+@end
---
+++ AirsoftLibrary/Model/GunItem.m
@@ -0,0 +1,13 @@
+//
+//  GunItem.m
+//  AirsoftLibrary
+//
+//  Created by kazumi hayashida on 2018/03/29.
+//  Copyright © 2018年 kh.inc.d. All rights reserved.
+//
+
+#import "GunItem.h"
+
+@implementation GunItem
+
+@end
