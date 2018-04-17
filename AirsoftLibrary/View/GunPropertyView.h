9a718c7e8ea9cc946a1f857840d061d6a9329b6c
markfour <axhfaro@gmail.com>
2018-04-17 16:12:42 +0900

    GunPropertyViewを追加

---
+++ AirsoftLibrary/View/GunPropertyView.h
@@ -0,0 +1,13 @@
+//
+//  GunPropertyView.h
+//  AirsoftLibrary
+//
+//  Created by kazumi hayashida on 2018/04/17.
+//  Copyright © 2018年 kh.inc.d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GunPropertyView : UIView
+
+@end
---
+++ AirsoftLibrary/View/GunPropertyView.m
@@ -0,0 +1,21 @@
+//
+//  GunPropertyView.m
+//  AirsoftLibrary
+//
+//  Created by kazumi hayashida on 2018/04/17.
+//  Copyright © 2018年 kh.inc.d. All rights reserved.
+//
+
+#import "GunPropertyView.h"
+
+@implementation GunPropertyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
