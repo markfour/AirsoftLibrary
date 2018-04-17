b9d33170ca5eda450194d0b841156abbffe71e77
markfour <axhfaro@gmail.com>
2018-04-17 16:44:57 +0900

    :hammer:ドッド記法に変更

--- AirsoftLibrary/Controller/RootViewController.m
+++ AirsoftLibrary/Controller/RootViewController.m
@@ -33,7 +33,7 @@
   GunItem *gunItem = _gunItems[indexPath.row];
   cell.displayNameLabel.text = gunItem.displayName;
   
-  GunPropertyView *gunProertyView = [[NSBundle mainBundle] loadNibNamed:@"GunPropertyView" owner:self options:nil][0];
+  GunPropertyView *gunProertyView = [NSBundle.mainBundle loadNibNamed:@"GunPropertyView" owner:self options:nil][0];
   gunProertyView.propertyLabel.text = @"パワーソース";
   gunProertyView.textLabel.text = gunItem.powerSouce;
   [cell.containerStackView addArrangedSubview:gunProertyView];
@@ -48,8 +48,8 @@
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout
   sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
   GunCollectionViewCell *cell = (GunCollectionViewCell *)[_collectionView cellForItemAtIndexPath:indexPath];
-//  NSLog(@"StackView height %f", cell.containerStackView.frame.size.height);
-
+  //  NSLog(@"StackView height %f", cell.containerStackView.frame.size.height);
+  
   CGFloat width = collectionView.frame.size.width;
   // TODO オートレイアウトを使用する　できるか?
   CGFloat height = 240;
