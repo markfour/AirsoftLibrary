5ed5e4f666e8fb6e733febb48154576662cebb79
markfour <axhfaro@gmail.com>
2018-05-19 17:25:06 +0900

    :tada: RootViewModelを追加

---
+++ AirsoftLibrary/ViewModel/RootViewModel.h
@@ -0,0 +1,13 @@
+//
+//  RootViewModel.h
+//  AirsoftLibrary
+//
+//  Created by kazumi hayashida on 2018/05/19.
+//  Copyright © 2018年 kh.inc.d. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RootViewModel : NSObject
+
+@end
---
+++ AirsoftLibrary/ViewModel/RootViewModel.m
@@ -0,0 +1,13 @@
+//
+//  RootViewModel.m
+//  AirsoftLibrary
+//
+//  Created by kazumi hayashida on 2018/05/19.
+//  Copyright © 2018年 kh.inc.d. All rights reserved.
+//
+
+#import "RootViewModel.h"
+
+@implementation RootViewModel
+
+@end
